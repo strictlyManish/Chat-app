c78dcd25ab59cd6afd15238d5a4e1ce026656466
strictlyManish <rajzmanish94@gmail.com>
2025-10-16 21:32:39 +0530

    update profile picture functionlity

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -90,23 +90,28 @@ export const logout = (req, res) => {
 };
 
 export const updateProfile = async (req, res) => {
-    try {
-        const { profilePic } = req.body;
-        const userid = req.user.id;
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
 
-        if (!profilePic) {
-            return res.status(400).json({ message: 'Profile pic required' })
-        }
-
-        const uploadResponse = await cloudinary.uploader.upload(profilePic)
-        const updateuser = await User.findByIdAndUpdate(userid, { profilePic: uploadResponse.secure_url }, { new: true });
-
-        res.status(200).json(updateuser)
-
-    } catch (error) {
-        res.status(500).json({ message: 'Internal server error' })
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
     }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
+
 
 export const checkAuth = (req, res) => {
     try {
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -9,7 +9,6 @@ export const useAuthStore = create((set) => ({
     isLoginIn: false,
     isUpdatingprofile: false,
 
-
     isCheckingAuth: true,
 
     checkAuth: async () => {
@@ -57,6 +56,20 @@ export const useAuthStore = create((set) => ({
             toast.success("Logged out successfully");
         } catch (error) {
             toast.error(error.response.data.message);
+        }
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({ authUser: res.data });
+            toast.success("Profile updated successfully");
+        } catch (error) {
+            console.log("error in update profile:", error);
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isUpdatingProfile: false });
         }
     },
 
