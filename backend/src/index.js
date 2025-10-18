f3d9aa8adea4dd23e7e4d3a7a56d76897910f450
strictlyManish <rajzmanish94@gmail.com>
2025-10-18 14:05:41 +0530

    ready for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,14 +5,15 @@ import { conectDB } from './lib/db.js';
 import cookie_parser from 'cookie-parser';
 import messageRoutes from './routes/message.route.js';
 import cors from 'cors';
+import path from 'path';
 
 import { app, server } from './lib/soket.js';
 
 
 dotenv.config();
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 
-    
 app.use(express.json());
 app.use(cookie_parser());
 
@@ -27,6 +28,12 @@ app.use(cors({
 app.use('/api/auth', authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist ')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+} 
 
 server.listen(PORT, () => {
     console.log('server runnig on port ' + PORT);
